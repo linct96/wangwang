11935dd14ce26ad6d4ca3ea7583877fcf2220c1d
linct96 <linct96@outlook.com>
2026-08-26 18:10:04 +0800

    chore: 初始化工程脚手架与构建配置

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { cloudflare } from '@cloudflare/vite-plugin'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/admin/',
+  build: {
+    outDir: 'dist/admin',
+  },
+  plugins: [react(), cloudflare()],
+})
